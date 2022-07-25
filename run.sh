851979afa1a678d76a349c9e82e6b745cc58dc73
Ivan Volodin <ivanwolodin@gmail.com>
2022-07-25 13:37:14 +0500

    Add: new functionality into sh-script

--- run.sh
+++ run.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Specify directory!'
+    exit 0
+fi
+
 timestamp() {
    date +%d-%m-%Y_%H-%M-%S
 }
+
+destination="${1}"
+mkdir $destination
 
 echo "Running..."
 echo "Creating dir"
 dirname=$(timestamp)
 
-mkdir "../txt_results/${dirname}"
-mkdir "../txt_results/${dirname}/res"
+mkdir "${destination}/${dirname}"
+mkdir "${destination}/${dirname}/res"
 
 echo "Compiling..."
 programname="$(timestamp).out"
 g++ -o ${programname} main.cpp
 
 echo "Moving to exec folder..."
-cp params.txt "../txt_results/${dirname}"
-mv ${programname} "../txt_results/${dirname}"
-cd ../txt_results/${dirname} || { echo "No such dir"; exit 1; }
+cp params.txt "${destination}/${dirname}"
+mv ${programname} "${destination}/${dirname}"
+cd ${destination}/${dirname} || { echo "No such dir"; exit 1; }
 
 echo "Executing in background..."
 ./${programname} &
